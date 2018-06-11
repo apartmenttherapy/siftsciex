dd4c460346168377f7a44951fb6c1d390c8f8015
Glen Holcomb <MentalLabor@gmail.com>
2018-06-11 15:29:47 -0600

    Tests for `mark`

--- test/siftsciex_test.exs
+++ test/siftsciex_test.exs
@@ -2,7 +2,11 @@ defmodule SiftsciexTest do
   use ExUnit.Case
   doctest Siftsciex
 
-  test "greets the world" do
-    assert Siftsciex.hello() == :world
+  test "mark/1 prepends a `$` to the given atom and returns an atom" do
+    assert :"$test" = Siftsciex.mark(:test)
+  end
+
+  test "mark/1 prepends a `$` to the given string and returns an atom" do
+    assert :"$test" = Siftsciex.mark("test")
   end
 end
