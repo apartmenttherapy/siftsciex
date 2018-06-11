782b28c5b79f55bee170d207983165f16918ec5e
Glen Holcomb <MentalLabor@gmail.com>
2018-06-11 15:28:33 -0600

    More generic `body` types

--- lib/siftsciex/body.ex
+++ lib/siftsciex/body.ex
@@ -1,3 +1,6 @@
 defmodule Siftsciex.Body do
   @type payload_string :: :empty | String.t
+  @type payload_time :: :empty | DateTime.t
+  @type payload_int :: :empty | integer
+  @type payload_float :: :empty | float
 end
