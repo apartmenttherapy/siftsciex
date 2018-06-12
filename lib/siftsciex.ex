4467fca072c4bca641dd7161d13d7fbaa3339bf4
Glen Holcomb <MentalLabor@gmail.com>
2018-06-11 21:21:35 -0600

    Adds `mark_string/1`

--- lib/siftsciex.ex
+++ lib/siftsciex.ex
@@ -3,5 +3,9 @@ defmodule Siftsciex do
   Documentation for Siftsciex.
   """
 
+  @doc false
   def mark(key), do: String.to_atom("$#{key}")
+
+  @doc false
+  def mark_string(value), do: "$#{value}"
 end
