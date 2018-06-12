e30ea3fe3b542d34f1c826ea41781e44c02690ac
Glen Holcomb <MentalLabor@gmail.com>
2018-06-11 21:23:23 -0600

    Tests for `mark_string/1`

--- test/siftsciex_test.exs
+++ test/siftsciex_test.exs
@@ -9,4 +9,12 @@ defmodule SiftsciexTest do
   test "mark/1 prepends a `$` to the given string and returns an atom" do
     assert :"$test" = Siftsciex.mark("test")
   end
+
+  test "mark_string/1 prepends a `$` to the given string" do
+    assert "$test" = Siftsciex.mark_string("test")
+  end
+
+  test "mark_string/1 prepends a `$` to the given atom and returns a string" do
+    assert "$test" = Siftsciex.mark_string(:test)
+  end
 end
