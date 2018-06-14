4b2d5bc34c59f8dc72312bb2e8eb3c6f9fa6064e
Glen Holcomb <MentalLabor@gmail.com>
2018-06-14 15:49:43 -0600

    Adds `create` for listing and account

--- lib/siftsciex/event.ex
+++ lib/siftsciex/event.ex
@@ -3,39 +3,132 @@ defmodule Siftsciex.Event do
   The Event module is used for registering events with SiftScience.
   """
 
-  alias Siftsciex.Validation
+  require Logger
 
+  alias Siftsciex.Event.{Account, Content, Response, Transport}
+
+  defstruct "$type": :empty, "$api_key": :empty, "$user_id": :empty, "$content_id": :empty, "$session_id": :empty, "$status": :empty, "$ip": :empty
   @type error_map :: %{required(String.t) => String.t}
+  @type result :: {:ok, Response.t}
+                  | {:error, :redirected, String.t}
+                  | {:error, :client_error, integer}
+                  | {:error, :server_error, integer}
+                  | {:error, :transport_error, any}
 
   @doc """
-  Registers an event with Sift Science, this function takes the event type along with a map containing the data for the event.  It returns `:ok` if the event is registered, `{:error, :badarg, %{errors: __MODULE__.error_map}` if a reqired field is missing, or `{:error, :trasport, reason}` if the connection to Sift Science fails for some reason.
+  Reports a `create_content.listing` event to Sift Science
 
   ## Parameters
 
-    - `event_type`: This is the type of event which is being registered, this is used to construct the request as well as validate the event data
-    - `data`: This is the event data, the event data should include all the specific values for the record you are reporting on
+    - `data`: The data for the newly created listing
+      * `:subject`
+      * `:body`
+      * `:contact_email`
+      * `:contact_address`
+      * `:locations`
+      * `:listed_items`
+      * `:images`
+      * `:expiration_time`
+      * `:ip`
+      * `:status`
+      * `:session_id`
+      * `:content_id`
+      * `:user_id`
 
   ## Examples
 
-      iex> Event.register("create_content.message", %{user_id: "bob", content_id: "2", status: :active, ip: "123.231.132.0", data: message})
-      :ok
+      iex> Event.create_listing(%{user_id: "bob", content_id: "8", status: :draft, listing: %{subject: "Chair", contact_address: %{name: "Walt", city: "Albuquerque"}, listed_items: [%{item_id: "8", price: 3, currency_code: "USD"}]}})
+      {:ok, %Siftsciex.Event.Response{}}
 
   """
-  @spec register(String.t, map) :: :ok
-                                   | {:error, :transport, map}
-                                   | {:error, :badarg, error_map}
-  def register(type, data) do
-    type
-    |> Validation.validate(data)
+  @spec create_listing(map) :: result
+  def create_listing(data) do
+    data
+    |> Content.create_listing()
+    |> purge_empty()
+    |> post()
+  end
+
+  @doc """
+  Register a `$create_account` Event with Sift Science
+
+  ## Parameters
+
+    - `data`: The account data that should be sent to Sift Science
+
+  ## Examples
+
+      iex> Event.create_account(%{user_id: "bob", user_email: "bob@example.com"})
+      {:ok, %Siftsciex.Event.Response{}}
+
+  """
+  @spec create_account(map) :: result
+  def create_account(data) do
+    data
+    |> Account.create_account()
+    |> purge_empty()
+    |> post()
+  end
+
+  defp post(payload) do
+    payload
+    |> Transport.post()
     |> case do
-         {:ok, payload} ->
-           post(payload)
-         {:error, errors} ->
-           {:error, :badarg, errors}
+         {:ok, %{status_code: 200} = response} ->
+           {:ok, Response.process(response.body())}
+         {:ok, %{status_code: status} = response} when status >= 300 and status <= 399 ->
+           {:error, :redirected, response.headers["Location"]}
+         {:ok, %{status_code: status}} when status >= 400 and status <= 499 ->
+           Logger.error("Failed to Post Event, received 4xx response for configured URI")
+           {:error, :client_error, status}
+         {:ok, %{status_code: status}} when status >= 500 and status <= 500 ->
+           {:error, :server_error, status}
+         {:error, error} ->
+           {:error, :transport_error, error.reason()}
        end
   end
 
-  def post(payload) do
-    #HTTPoison.post(event_url(), payload)
+  @doc """
+  Purges all keys with `:empty` values from the given map.
+
+  ## Parameters
+
+    - `record`: The record that should be purged
+
+  ## Examples
+
+      iex> Event.purge_empty(%{"$type": "$create_content", "$ip": :empty})
+      %{"$type": "$create_content"}
+
+      iex> Event.purge_empty(%{"$type": "$create_content", "$listing": %{"$subject": "Table", "$contact_address": :empty}})
+      %{"$type": "$create_content", "$listing": %{"$subject": "Table"}}
+
+      iex> Event.purge_empty(%{"$type": "$create_content", "$listing": %{"$listed_items": [%{"$price": 5000000, "$currency_code": :empty}]}})
+      %{"$type": "$create_content", "$listing": %{"$listed_items": [%{"$price": 5000000}]}}
+
+  """
+  @spec purge_empty(map) :: map
+  def purge_empty(%_{} = record) do
+    record
+    |> Map.from_struct()
+    |> purge_empty()
+  end
+  def purge_empty(record) do
+    Enum.reduce(record, %{}, fn {key, value}, stripped ->
+      case value do
+        %{} = value ->
+          Map.put(stripped, key, purge_empty(value))
+        value when is_list(value) ->
+          values =
+            value
+            |> Enum.map(&purge_empty/1)
+
+          Map.put(stripped, key, values)
+        :empty ->
+          stripped
+        value ->
+          Map.put(stripped, key, value)
+      end
+    end)
   end
 end
--- test/siftsciex/event/event_test.exs
+++ test/siftsciex/event/event_test.exs
@@ -2,6 +2,29 @@ defmodule Siftsciex.EventTest do
   use ExUnit.Case
 
   alias Siftsciex.Event
+  alias Siftsciex.Event.Response
 
-  doctest Event
+  doctest Event, except: [create_account: 1, create_listing: 1]
+
+  test "create_account/1 sends a $create_account Event to Sift Science" do
+    assert {:ok, %Response{}} = Event.create_account(account_data())
+  end
+
+  test "create_listing/1 sends a $create_content -> $listing Event to Sift Science" do
+    assert {:ok, %Response{}} = Event.create_listing(listing_data())
+  end
+
+  def account_data, do: %{user_id: "bob", user_email: "bob@example.com"}
+
+  def listing_data do
+    %{user_id: "bob",
+      content_id: "8",
+      status: :draft,
+      listing: %{subject: "Chair",
+        contact_address: %{name: "Walt",
+          city: "Albuquerque"},
+        listed_items: [%{item_id: "8", price: 3, currency_code: "USD"}]
+      }
+    }
+  end
 end
