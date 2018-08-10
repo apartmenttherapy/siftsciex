6159390b2ad98bcdf3b4e573b4b03f2bea882816
Glen Holcomb <MentalLabor@gmail.com>
2018-08-10 09:56:04 -0600

    version bump

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Siftsciex.MixProject do
       name: "Siftsciex",
       description: description(),
       package: package(),
-      version: "0.3.1",
+      version: "0.4.0",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env),
       test_coverage: [tool: ExCoveralls],
