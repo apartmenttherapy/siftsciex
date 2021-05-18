51f803385cc41619eaa738569fa7b8188b67e254
Marcos Ramos <rms.mrcs@gmail.com>
2021-05-18 18:03:51 -0300

    Sync workflows support

---
+++ test/siftsciex/score/response/workflow_status/entity_test.exs
@@ -0,0 +1,7 @@
+defmodule Siftsciex.Score.Response.WorkflowStatus.EntityTest do
+  use ExUnit.Case
+
+  alias Siftsciex.Score.Response.WorkflowStatus.Entity
+
+  doctest Entity
+end
---
+++ test/siftsciex/score/response/workflow_status_test.exs
@@ -0,0 +1,7 @@
+defmodule Siftsciex.Score.Response.WorkflowStatusTest do
+  use ExUnit.Case
+
+  alias Siftsciex.Score.Response.WorkflowStatus
+
+  doctest WorkflowStatus
+end
